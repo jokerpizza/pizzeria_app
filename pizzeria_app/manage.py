470d07860f9f5c355b61b90195d765e08f653ccc
jokerpizza <kamil.matlega@gmail.com>
2025-07-22 15:30:08 +0200

    Delete pizzeria_app directory

--- pizzeria_app/manage.py
+++
@@ -1,9 +0,0 @@
-from cost_core import create_app
-from cost_core.extensions import db
-from flask_migrate import Migrate, upgrade
-
-app = create_app()
-migrate = Migrate(app, db)
-
-if __name__ == "__main__":
-    app.run()
