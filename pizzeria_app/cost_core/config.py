42f7566ef15c9419310c1ffbfc1ef55bb983dbd8
jokerpizza <kamil.matlega@gmail.com>
2025-07-22 15:56:28 +0200

    Delete pizzeria_app/cost_core directory

--- pizzeria_app/cost_core/config.py
+++
@@ -1,5 +0,0 @@
-import os
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///dev.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "secret")
