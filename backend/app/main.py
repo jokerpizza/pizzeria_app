aabb583ee7efe2b76a306b81fdb58ccd9e426cdf
jokerpizza <kamil.matlega@gmail.com>
2025-07-23 17:12:40 +0200

    Delete backend directory

--- backend/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .routers import products, recipes, dashboard, categories
-
-app = FastAPI()
-
-app.include_router(products.router)
-app.include_router(recipes.router)
-app.include_router(dashboard.router)
-app.include_router(categories.router)
