39a1edc9ad5fd630e9108bfb917bb472ee400881
jokerpizza <kamil.matlega@gmail.com>
2025-07-23 16:33:52 +0200

    Delete backend directory

--- backend/app/config.py
+++
@@ -1,7 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./app.db"
-    ALLOW_ORIGINS: list[str] = ["*"]
-
-settings = Settings()
