5b77b88cff0fc55762108a599b2247753c318c5e
jokerpizza <kamil.matlega@gmail.com>
2025-07-23 13:41:16 +0200

    Add files via upload

--- backend/app/sales/scheduler.py
+++ backend/app/sales/scheduler.py
@@ -1,16 +1,12 @@
 
 import asyncio
-import datetime as dt
-from .ingestor_rest import fetch_orders_period, store_orders
+from .ingestor_rest import fetch_and_store_period
 
 async def scheduler():
     while True:
         try:
-            before = dt.datetime.utcnow()
-            after = before - dt.timedelta(minutes=5)
-            rows = await fetch_orders_period(after, before)
-            new_items = await store_orders(rows)
+            new_items = await fetch_and_store_period()
             print(f"Fetched {new_items} new order items")
         except Exception as e:
             print("Scheduler error:", e)
-        await asyncio.sleep(300)
+        await asyncio.sleep(300)  # 5 minutes
