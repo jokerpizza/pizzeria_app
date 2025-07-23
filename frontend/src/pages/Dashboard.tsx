f084bfc0a433e2a927f1064592b28dc7782fbef7
jokerpizza <kamil.matlega@gmail.com>
2025-07-23 16:33:59 +0200

    Delete frontend directory

--- frontend/src/pages/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Dashboard: React.FC = () => (
-  <div>
-    <h1 style={{fontSize:'1.5rem', fontWeight:600, marginBottom:'1rem'}}>Dashboard</h1>
-    <p>Witaj w aplikacji FoodCost. Wybierz sekcję z menu.</p>
-  </div>
-);
-export default Dashboard;
