40e4c4590c50b3930175842c46b8dc609645e50d
jokerpizza <kamil.matlega@gmail.com>
2025-07-23 14:33:38 +0200

    Delete backend directory

--- backend/app/utils.py
+++
@@ -1,7 +0,0 @@
-def to_kg(amount:float, unit:str)->float:
-    if unit=='kg': return amount
-    if unit=='g': return amount/1000
-    if unit=='l': return amount
-    if unit=='ml': return amount/1000
-    if unit=='szt': return amount
-    raise ValueError('unknown unit')
--- backend/sql/add_categories.sql
+++
@@ -1,9 +0,0 @@
--- Dodaj nową tabelę kategorii
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
-);
-
--- Dodaj pole category_id do tabeli przepisów
-ALTER TABLE recipes
-    ADD COLUMN category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL;
