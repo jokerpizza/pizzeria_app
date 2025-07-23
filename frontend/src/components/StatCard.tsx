28169d26983cd071236e49e553316186329518e7
jokerpizza <kamil.matlega@gmail.com>
2025-07-23 14:33:44 +0200

    Delete frontend directory

--- frontend/src/components/StatCard.tsx
+++
@@ -1,6 +0,0 @@
-export default function StatCard({title,value}:{title:string,value:any}){
-  return <div className="bg-white shadow rounded p-4">
-    <div className="text-sm text-gray-500">{title}</div>
-    <div className="text-2xl font-bold">{value}</div>
-  </div>
-}
