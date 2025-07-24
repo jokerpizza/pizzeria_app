0293037f024354f493ee9a422e1399fd85450fc7
jokerpizza <kamil.matlega@gmail.com>
2025-07-25 01:17:18 +0200

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import IngredientList from './components/IngredientList'
+import RecipeList from './components/RecipeList'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex-1 bg-gray-100 p-4 overflow-auto">
+          <Routes>
+            <Route path="/" element={<h1 className="text-2xl font-bold">Dashboard</h1>} />
+            <Route path="/ingredients" element={<IngredientList />} />
+            <Route path="/recipes" element={<RecipeList />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
